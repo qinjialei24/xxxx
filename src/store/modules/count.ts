53ddf3d0f0ad147cb5bb1418dd3aa84700528534
jialei qin <jialei.qin@thoughtworks.com>
2021-06-02 18:15:04 +0800

    feat: remove createModel

--- src/store/modules/count.ts
+++ src/store/modules/count.ts
@@ -1,37 +1,33 @@
 import childModule, {ChildModule} from "./child1";
-import {createModel} from "../../redux-brief";
 
-export interface CountModule{
-    namespace:'count'
-    state:{
-        money:number
+export interface CountModule {
+    namespace: 'count'
+    state: {
+        money: number
     }
-    reducer:{
-        add: (payload:number,state:CountModule['state']) =>void
-        minus: (payload:number,state:CountModule['state']) =>void
+    reducer: {
+        add: (payload: number, state: CountModule['state']) => void
+        minus: (payload: number, state: CountModule['state']) => void
     }
 }
 
-
-  const countModule:CountModule & ChildModule = {
-    namespace:'count',
-    state:{
-        money:10,
+const countModule: CountModule & ChildModule = {
+    namespace: 'count',
+    state: {
+        money: 10,
         ...childModule.state
     },
-    reducer:{
-        add(payload,state){
+    reducer: {
+        add(payload, state) {
             console.log("-> payload", payload);
 
-            state.money+=payload
+            state.money += payload
         },
-        minus(payload,state){
-            state.money-=payload
+        minus(payload, state) {
+            state.money -= payload
         },
         ...childModule.reducer
     }
 }
-
-// const countModel = createModel(countModule);
 
 export default countModule
