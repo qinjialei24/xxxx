26c5dd4e49951df2a9be2fb4ee7d64f7701b583b
jialei qin <jialei.qin@thoughtworks.com>
2021-06-02 09:36:38 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import { useSelector,useDispatch } from 'react-redux'
 
-import store, {reducerMap} from "./store";
+// import store, {reducerMap} from "./store";
 
 function App() {
-  // const [todos, setTodos] = React.useState(store.getState().todos);
-  store.subscribe(() => {
-    // setTodos(store.getState().todos)
-      console.log("-> store.getState()", store.getState());
-  })
+    const todos =useSelector((state:any)=>{
+        console.log("-> state", state);
+
+        return state.todos
+    });
+    const dispatch = useDispatch()
 
   return (
     <div className="App">
       <button onClick={() => {
-          reducerMap.count.add(1)
-
-        //   store.dispatch({
-        //   type:'ADD_TODO',
-        //   text:'1111'
-        // })
-
+          // reducerMap.count.add(1)
+          const action ={
+              type:'ADD_TODO',
+              text:'1111'
+          }
+          dispatch(action)
       }}>add todo</button>
 
-      <h1>todos:</h1>
-      {/*<h2>{todos}</h2>*/}
+        <h1>todos</h1>
+        {todos}
+
     </div>
   );
 }
